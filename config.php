8fc6cdd8b7797caa82f18eac60a12d64708a912d
hseki <hal@HARUNA-no-MacBook-Air.local>
2017-02-10 22:34:16 +0900

    設定ファイル追加

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Twitter APIキー設定
+$consumer_key = '××××××××××××××××××××';
+$consumer_secret = '××××××××××××××××××××';
+$access_token = '××××××××××××××××××××';
+$access_token_secret = '××××××××××××××××××××';
+
+define('CONSUMER_KEY', $consumer_key);
+define('CONSUMER_SECRET', $consumer_secret);
+define('ACCESS_TOKEN', $access_token);
+define('ACCESS_TOKEN_SECRET', $access_token_secret);
+?>
